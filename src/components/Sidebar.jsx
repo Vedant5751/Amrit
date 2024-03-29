e7b9bc32fe49d4ea4b80882c72e8924395e03c4d
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-03-29 14:32:16 +0530

    minor changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -43,7 +43,7 @@ export default function Sidebar() {
               >
                 <img src="./NavLogo.svg" className="h-8 me-3" alt="Logo" />
                 <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                  Vikiran
+                  Amrit
                 </span>
               </a>
             </div>
