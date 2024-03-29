0e5c7fffaec3cace522608ff986d3dba88d0db16
Vedant Patil <vedant1346@gmail.com>
2024-03-29 11:30:51 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-400 font-bold underline">Hello world!</h1>
+      <Router>
+        <Routes>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
