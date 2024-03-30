91208d7fbafe7b7f9c3b635d59a8bb1b43a2f734
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-03-30 08:48:19 +0530

    firebase pic upload

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getDatabase } from "firebase/database"; 
+import { getStorage } from "firebase/storage"; // Add this import
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,6 +18,7 @@ const firebaseConfig = {
   messagingSenderId: "998439364766",
   appId: "1:998439364766:web:44ab5ebb1022cbf3c1338f",
   // databaseURL: "https://vikiran-28f65-default-rtdb.firebaseio.com/",
+  appId: "1:998439364766:web:44ab5ebb1022cbf3c1338f",
 };
 
 // Initialize Firebase
@@ -23,5 +26,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 // const database = getDatabase(app);
+const storage = getStorage(app); // Initialize Firebase Storage
 
-export { app, auth, db, };
+
+export { app, auth, db, storage };
