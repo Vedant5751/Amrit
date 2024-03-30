e43b22e83f51642b0323063abfaa8f0416da6e89
Vedant Patil <vedant1346@gmail.com>
2024-03-30 09:21:53 +0530

    Nav Change

--- src/components/floating-navbar.tsx
+++ src/components/floating-navbar.tsx
@@ -50,12 +50,12 @@ export const FloatingNav = ({ className }: { className?: string }) => {
           className
         )}
       >
-        <Link to="/patienthome">
+        {/* <Link to="/patienthome">
           <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
             <span className="hover:text-blue-500">Home</span>
             <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
           </button>
-        </Link>
+        </Link> */}
         <Link to="/patientreport">
           <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
             <span className="hover:text-blue-500">Reports</span>
